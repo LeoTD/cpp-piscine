e2598bda0389db4cb898cb02a717c5799e32a4be
Leo Tanenbaum-Diaz <leo.tanenbaum.diaz@gmail.com>
2018-10-01 13:26:59 -0700

    OMG I KNOW CPP 8D

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,1 +1,22 @@
-//I don't know what I'm doing
+#include <iostream>
+using namespace std;
+
+char	to_upper(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c + ('A' - 'a'));
+	else
+		return (c);
+}
+
+int		main(int ac, char **av)
+{
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j] != '\0'; j++)
+		{
+			av[i][j] = to_upper(av[i][j]);
+		}
+		cout << av[i] << endl;
+	}
+}
